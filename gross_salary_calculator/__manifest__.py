7d3c82e933bb35c229a692c0c49cff300f00a423
abdullahhh123 <abdullahmahmoud388@gmai.com>
2025-05-29 20:07:42 +0300

    adding v.17

---
+++ gross_salary_calculator/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "odoo_version": "18.0",
+    "name": "Gross Salary Calculator",
+    "author": "Osama Moawad",
+    "version": "1.3",
+    "summary": "Calculate gross salary from net",
+    "category": "Human Resources",
+    "depends": ["hr"],
+    "data": [
+        "views/hr_contract_views.xml"
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3"
+}
